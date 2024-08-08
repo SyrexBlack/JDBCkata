df8495571f58360f383c642bfbe6a5933d8f6223
Syrexblack <grigoriy002@yandex.ru>
2024-08-08 17:09:05 +0500

    Completed

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,43 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.SQLException;
 import java.util.List;
 
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl extends Util implements UserService {
+    private UserDao userDaoJDBC = new UserDaoJDBCImpl();
+    @Override
     public void createUsersTable() {
-
+        userDaoJDBC.createUsersTable();
     }
-
+    @Override
     public void dropUsersTable() {
-
+        userDaoJDBC.dropUsersTable();
     }
-
+    @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        userDaoJDBC.saveUser(name,lastName,age);
+        System.out.println("User: " + name + " добавлен в базу данных");
     }
-
+    @Override
     public void removeUserById(long id) {
-
+        userDaoJDBC.removeUserById(id);
     }
-
+    @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> users =  userDaoJDBC.getAllUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
+        return users;
     }
-
-    public void cleanUsersTable() {
-
+    @Override
+    public void cleanUsersTable() throws SQLException {
+        userDaoJDBC.cleanUsersTable();
     }
 }
+
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:/kata";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "8305";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("Ну типа подключился, и чо?");
+            }
+        } catch (SQLException e) {
+            System.out.println("Нет подключения");
+            e.printStackTrace();  // Добавляем вывод стека исключений
+        }
+        return connection;
+    }
 }
